58542ea717098239a0f0b6db3ef7225a39874c8a
WangYifan1024 <147827414+WangYifan1024@users.noreply.github.com>
2024-05-02 12:09:36 +0800

    Update the table

--- Driver.py
+++ Driver.py
@@ -79,13 +79,34 @@ class StudentList:
                 return
         print("未找到该学生信息！")
 
+    def update(self, id):
+        student = self.find_by_id(id)
+        if student:
+            print("学生信息已找到：")
+            print("学号：", student.id)
+            print("姓名：", student.name)
+            print("语文成绩：", student.Chinese)
+            print("数学成绩：", student.Math)
+            print("英语成绩：", student.English)
+            print("请输入更新后的信息：")
+            student.name = input("姓名：")
+            student.Chinese = self.__enterScore("语文成绩：")
+            student.Math = self.__enterScore("数学成绩：")
+            student.English = self.__enterScore("英语成绩：")
+            print("学生信息已更新！")
+        else:
+            print("未找到该学生信息！")
+
+
     def show_menu(self):
         print("\n" + "-" * 40)
         print("|{: ^30}|".format("学生基本信息管理系统"))
         print("-" * 40)
         print("|{:<3} {:<30}|".format("1)", "展示学生信息"))
         print("|{:<3} {:<30}|".format("2)", "添加学生信息"))
-        print("|{:<3} {:<31}|".format("3)", "结束系统"))
+        print("|{:<3} {:<24}|".format("3)", "根据学号查找、删除、更新学生信息"))
+        print("|{:<3} {:<26}|".format("4)", "根据姓名关键字查找学生信息"))
+        print("|{:<3} {:<31}|".format("5)", "结束系统"))
         print("-" * 40)
 
     def infoProcess(self):
@@ -98,6 +119,39 @@ class StudentList:
                 self.add()
             elif s == "3":
                 break
+            elif s == "4":
+                id = input("请输入学生学号：")
+                action = input("请选择操作（find/delete/update）：").strip().lower()
+                if action == "find":
+                    student = self.find_by_id(id)
+                    if student:
+                        print("学生信息已找到：")
+                        print("学号：", student.id)
+                        print("姓名：", student.name)
+                        print("语文成绩：", student.Chinese)
+                        print("数学成绩：", student.Math)
+                        print("英语成绩：", student.English)
+                    else:
+                        print("未找到该学生信息！")
+                elif action == "delete":
+                    self.delete(id)
+                elif action == "update":
+                    self.update(id)
+                else:
+                    print("无效操作！")
+            elif s == "5":
+                keyword = input("请输入要查找的姓名关键字：")
+                results = self.find_by_name_keyword(keyword)
+                if results:
+                    print("查找结果：")
+                    for student in results:
+                        print("学号：", student.id)
+                        print("姓名：", student.name)
+                        print("语文成绩：", student.Chinese)
+                        print("数学成绩：", student.Math)
+                        print("英语成绩：", student.English)
+                else:
+                    print("未找到匹配的学生信息！")
             else:
                 print("\033[91m输入错误\033[0m")
 
