f26e11676c0fd0a882846831e9051a517b81d6c4
nxdxwj <202383910029@nuist.edu.cn>
2024-05-14 11:43:33 +0800

    update

--- StudentList.py
+++ StudentList.py
@@ -6,17 +6,70 @@ class StudentList:
     def __init__(self):
         self.studentList = []
 
+    def infoProcess(self):
+        while True:
+            self.show_menu()
+            s = input("\033[94minfo==> \033[0m").strip().lower()
+            if s == "1":
+                self.show()
+            elif s == "2":
+                self.add()
+            elif s == "7":
+                break
+            elif s == "3":
+                id = int(input("请输入学生学号："))
+                action = input("请选择操作（find/delete/update）：").strip().lower()
+                if action == "find":
+                    student = self.find_by_id(id)
+                    if student:
+                        print("学生信息已找到：")
+                        print("学号：", student.id)
+                        print("姓名：", student.name)
+                        print("语文成绩：", student.Chinese)
+                        print("数学成绩：", student.Math)
+                        print("英语成绩：", student.English)
+                    else:
+                        print("未找到该学生信息！")
+                elif action == "delete":
+                    self.delete(id)
+                elif action == "update":
+                    self.update_by_id(id)
+                else:
+                    print("无效操作！")
+            elif s == "4":
+                keyword = input("请输入要查找的姓名关键字：")
+                results = self.find_by_name_keyword(keyword)
+                if results:
+                    print("查找结果：")
+                    for student in results:
+                        print("学号：", student.id)
+                        print("姓名：", student.name)
+                        print("语文成绩：", student.Chinese)
+                        print("数学成绩：", student.Math)
+                        print("英语成绩：", student.English)
+                else:
+                    print("未找到匹配的学生信息！")
+            elif s == "5":
+                self.save()
+                print("保存成功")
+            elif s == "6":
+                self.load()
+                print("导入成功!")
+
+            else:
+                print("\033[91m输入错误\033[0m")
+            input("\nPress enter key to continue...")
     def show(self):
-        print("{:8}\t{:8}\t{:8}\t{:8}\t{:8}"
+        print("{:<8}\t{:<8}\t{:<8}\t{:<8}\t{:<8}"
               .format("学号", "姓名", "语文", "数学", "英语"))
         for student in self.studentList:
-            print('{:8}\t{:8}\t{:<8}\t{:<8}\t{:<8}'
+            print('{:<8}\t{:8}\t{:<8}\t{:<8}\t{:<8}'
                   .format(student.id, student.name, student.Chinese, student.Math, student.English))
 
-    def __enterScore(self, message):
+    def __enterScore(self, score):
         while True:
             try:
-                score = int(input(message))
+                score = int(input(score))
                 if 0 <= score <= 100:
                     return score
                 else:
@@ -24,12 +77,14 @@ class StudentList:
             except ValueError:
                 print("输入错误，请输入一个整数")
 
+    #按照 id 查看学生是否存在
     def __exists(self, id):
         for student in self.studentList:
             if student.id == id:
                 return True
         return False
 
+    # 按照 name 查看学生是否存在
     def __exists(self, name):
         for student in self.studentList:
             if student.name == name:
@@ -74,7 +129,7 @@ class StudentList:
                 return
         print("未找到该学生信息！")
 
-    def update(self, id):
+    def update_by_id(self, id):
         student = self.find_by_id(id)
         if student:
             print("学生信息已找到：")
@@ -94,79 +149,21 @@ class StudentList:
 
     def show_menu(self):
         print("\n" + "-" * 40)
-        print("|{: ^30}|".format("学生基本信息管理系统"))
+        print("{: ^30}".format("学生基本信息管理系统"))
         print("-" * 40)
-        print("|{:<3} {:<30}|".format("1)", "展示学生信息"))
-        print("|{:<3} {:<30}|".format("2)", "添加学生信息"))
-        print("|{:<3} {:<24}|".format("3)", "根据学号查找、删除、更新学生信息"))
-        print("|{:<3} {:<26}|".format("4)", "根据姓名关键字查找学生信息"))
-        print("|{:<3} {:<31}|".format("5)", "保存数据"))
-        print("|{:<3} {:<31}|".format("6)", "导入数据"))
-        print("|{:<3} {:<31}|".format("7)", "结束系统"))
+        print(" {:<3} {:<30}|".format("1)", "展示学生信息"))
+        print(" {:<3} {:<30}|".format("2)", "添加学生信息"))
+        print(" {:<3} {:<23}|".format("3)", "根据学号查找、删除、更新学生信息"))
+        print(" {:<3} {:<25}|".format("4)", "根据姓名关键字查找学生信息"))
+        print(" {:<3} {:<31}|".format("5)", "保存数据"))
+        print(" {:<3} {:<31}|".format("6)", "导入数据"))
+        print(" {:<3} {:<31}|".format("7)", "结束系统"))
         print("-" * 40)
 
-    def infoProcess(self):
-        while True:
-            self.show_menu()
-            s = input("\033[94minfo==> \033[0m").strip().lower()
-            if s == "1":
-                self.show()
-            elif s == "2":
-                self.add()
-            elif s == "7":
-                break
-            elif s == "3":
-                id = input("请输入学生学号：")
-                action = input("请选择操作（find/delete/update）：").strip().lower()
-                if action == "find":
-                    student = self.find_by_id(id)
-                    if student:
-                        print("学生信息已找到：")
-                        print("学号：", student.id)
-                        print("姓名：", student.name)
-                        print("语文成绩：", student.Chinese)
-                        print("数学成绩：", student.Math)
-                        print("英语成绩：", student.English)
-                    else:
-                        print("未找到该学生信息！")
-                elif action == "delete":
-                    self.delete(id)
-                elif action == "update":
-                    self.update(id)
-                else:
-                    print("无效操作！")
-            elif s == "4":
-                keyword = input("请输入要查找的姓名关键字：")
-                results = self.find_by_name_keyword(keyword)
-                if results:
-                    print("查找结果：")
-                    for student in results:
-                        print("学号：", student.id)
-                        print("姓名：", student.name)
-                        print("语文成绩：", student.Chinese)
-                        print("数学成绩：", student.Math)
-                        print("英语成绩：", student.English)
-                else:
-                    print("未找到匹配的学生信息！")
-            elif s == "5":
-                self.save()
-                print("保存成功")
-            elif s == "6":
-                self.load()
-                print("导入成功")
 
-            else:
-                print("\033[91m输入错误\033[0m")
 
     def save(self):
-        columns = ["学号", "姓名", "语文", "数学", "英语"]
-        id_list = []
-        name_list = []
-        chinese_list = []
-        math_list = []
-        english_list = []
-        data = [[student.id, student.name, student.Chinese, student.Math, student.English] for student in
-                self.studentList]
+        data = [[student.id, student.name, student.Chinese, student.Math, student.English] for student in self.studentList]
         df = pd.DataFrame(data, columns=["学号", "姓名", "语文", "数学", "英语"])
         # 将数据写入Excel文件
         df.to_excel('studentlist.xlsx', index=False)
